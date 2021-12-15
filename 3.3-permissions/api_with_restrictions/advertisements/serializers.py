4f2a397aa73d36f5974f2e52be1a088aaee98818
Геннадий <g.utkin80@yandex.ru>
2021-12-15 21:08:46 +0300

    3.3-permissions

--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
+from rest_framework.exceptions import ValidationError
 from advertisements.models import Advertisement
 
 
@@ -41,8 +41,9 @@ class AdvertisementSerializer(serializers.ModelSerializer):
         """Метод для валидации. Вызывается при создании и обновлении."""
 
         # TODO: добавьте требуемую валидацию
-        user = self.context['request'].user
-        if Advertisement.objects.filter(status='OPEN', creator=user).count() >= 10:
-            raise serializers.ValidationError('10')
+        if data.get("status", None) == "OPEN":
+            list_open = Advertisement.objects.filter(creator=self.context["request"].user, status="OPEN")
+            if len(list_open) >= 10:
+                raise ValidationError("Превышен лимит объявлений в статусе OPEN - 10 шт.")
 
         return data
